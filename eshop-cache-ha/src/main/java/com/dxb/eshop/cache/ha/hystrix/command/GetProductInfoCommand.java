7c64c94094df54274e3b171e0fe54860fd0db553
戴晓波 <daixiaobo@wondersoft.cn>
2019-06-12 14:35:47 +0800

    小改

--- eshop-cache-ha/src/main/java/com/dxb/eshop/cache/ha/hystrix/command/GetProductInfoCommand.java
+++ eshop-cache-ha/src/main/java/com/dxb/eshop/cache/ha/hystrix/command/GetProductInfoCommand.java
@@ -13,7 +13,16 @@ public class GetProductInfoCommand extends HystrixCommand<ProductInfo> {
 
     public GetProductInfoCommand(Long productId) {
         super(HystrixCommand.Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("GetProductInfoCommandGroup"))
-                .andCommandPropertiesDefaults(HystrixCommandProperties.Setter().withExecutionTimeoutInMilliseconds(2500))
+                .andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
+                        //超时
+                        .withExecutionTimeoutEnabled(true)
+                        .withExecutionTimeoutInMilliseconds(2500)
+                        //滑动窗口中，最少要有多少个请求时，才触发开启短路
+                        .withCircuitBreakerRequestVolumeThreshold(20)
+                        //当异常请求达到这个百分比时，就触发打开短路器
+                        .withCircuitBreakerErrorThresholdPercentage(50)
+                        //再重新导holf-open状态
+                        .withCircuitBreakerSleepWindowInMilliseconds(5000))
                 .andCommandKey(GETTER_KEY));
         this.productId = productId;
     }
